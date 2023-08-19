e85832b366a44f28085fc0a940b48be4b12a3f7c
mohanadft <mohanadfteha@gmail.com>
2023-08-20 01:24:43 +0300

    :sparkles: Implement the most efficient solution

--- src/fibonacci/tabulation.ts
+++ src/fibonacci/tabulation.ts
@@ -52,3 +52,29 @@ const solution2 = (n: number): number => {
 
   return dp.at(-1) as number;
 };
+
+/**
+ * The most optimized solution which has a
+ * TC: O(n)
+ * SC: O(1)
+ *
+ * This solution come with wondering, why we need a whole array for computing
+ * the fib of a number, while we just need two values which they are the values
+ * before the number and twice before it.
+ * @param {number} n
+ * @returns {number}
+ */
+
+const soultion3 = (n: number): number => {
+  let prev1 = 0;
+  let prev2 = 1;
+  let res = 0;
+
+  for (let i = 2; i <= n; i++) {
+    res = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = res;
+  }
+
+  return res;
+};
