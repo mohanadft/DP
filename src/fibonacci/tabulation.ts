75aae780c159c04e431dbf55d84109de27dd3bb9
mohanadft <mohanadfteha@gmail.com>
2023-08-19 23:59:42 +0300

    :sparkles: Making two file each one has a type of solution

---
+++ src/fibonacci/tabulation.ts
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+
+// Solution 5 (Generators).
+// This approach `Tabulation` starts from base cases to the result. (In opposite of memoization)
+const solution5 = (n: number): number => {
+  function* fibGenerator() {
+    let arr = [0, 1];
+
+    yield arr[0];
+    yield arr[1];
+
+    while (true) {
+      let next = arr.at(-1)! + arr.at(-2)!;
+      arr.push(next);
+      yield next;
+    }
+  }
+
+  let gen = fibGenerator();
+
+  for (let i = 0; i < n; i++) {
+    gen.next();
+  }
+
+  return gen.next().value as number;
+};
