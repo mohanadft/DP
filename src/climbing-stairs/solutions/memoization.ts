127b2d29da518fb8c45262edbe6aafe1ac08c988
mohanadft <mohanadfteha@gmail.com>
2023-08-24 01:14:41 +0300

    :sparkles: Add `solution2`

--- src/climbing-stairs/solutions/memoization.ts
+++ src/climbing-stairs/solutions/memoization.ts
@@ -19,3 +19,22 @@ const solution = (n: number): number => {
   };
   return backtrack(n);
 };
+
+/**
+ *
+ * @param {number} n
+ * @param {MemoObjectType} m
+ * @returns {number}
+ */
+
+// Solution 2 (Having a default hash,
+// no need to declare it in scope of the function)
+
+const solution2 = (n: number, m: MemoObjectType = {}): number => {
+  if (n <= 1) return 1;
+
+  if (m[n]) return m[n];
+
+  m[n] = solution2(n - 1, m) + solution2(n - 2, m);
+  return m[n];
+};
