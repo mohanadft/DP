3410749d8b08d54381799591ec30ae3c67d30c8c
mohanadft <mohanadfteha@gmail.com>
2023-09-08 01:04:42 +0300

    :sparkles: Add the 2nd solution, an enhancement of the before one

--- src/frog-jump/solutions/memoization.ts
+++ src/frog-jump/solutions/memoization.ts
@@ -20,3 +20,36 @@ const fragJump1 = (n: number, heights: number[]): number => {
 
   return Math.min(jumpOne, jumpTwo);
 };
+
+/**
+ * An Optimized Solution of the above.
+ *
+ * @param {number} n
+ * @param {number[]} heights
+ * @param {number[]} dp
+ * @returns {number}
+ */
+
+const fragJump2 = (
+  n: number,
+  heights: number[],
+  dp: number[] = Array(n + 1).fill(-1)
+): number => {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (dp[n] !== -1) return dp[n];
+
+  let jumpOne =
+    fragJump2(n - 1, heights, dp) + Math.abs(heights[n] - heights[n - 1]);
+  let jumpTwo = Infinity;
+
+  if (n > 1) {
+    jumpTwo =
+      fragJump2(n - 2, heights, dp) + Math.abs(heights[n] - heights[n - 2]);
+  }
+
+  dp[n] = Math.min(jumpOne, jumpTwo);
+  return dp[n];
+};
